34e976614289f9c9aa27b094544d1a3fc60d4fc6
shayankiyani <shayan76kiyani@gmail.com>
2020-03-06 11:58:56 +0330

    add timeZone

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var jdate = require('jdate').JDate;
 
 var package = {} ; 
 
-package.date = function (format){
-    return jdate().toString(format); 
+package.date = function (time, timeZone, format){
+    if (time && timeZone) {
+        return jdate(time.toLocaleString('en-GB', { timeZone: timeZone })).toString(format);
+    } else if (timeZone) {
+        return jdate(new Date().toLocaleString('en-GB', { timeZone: timeZone })).toString(format);
+    } else {
+        return jdate(time).toString(format);
+    }
 }
 
 function convert(dateString , type){
@@ -16,6 +22,16 @@ function convert(dateString , type){
             // convert to miladi
             arrNewDate = jdate.convert().toGregorian(arrSourceDate);
         }
+
+        // make month and days 2 digit 
+        arrNewDate = [arrNewDate[0], twoDigit(arrNewDate[1]), twoDigit(arrNewDate[2])]
+        var twoDigit = function(value) {
+            value += '';
+            if (value.length < 2) {
+                value = '0' + value;
+            }
+            return value;
+        };
 
         return arrNewDate.join('/') + dateString.substr(10,9);
     } catch (error) {
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 var shamsiDate = require('./index');
 
-console.log(shamsiDate.date('yyyy/MM/dd'));// 1398/09/04 
-console.log(shamsiDate.date('yyyy/MM/dd HH:mm:ss'));// 1398/09/04 09:05:51
+console.log(shamsiDate.date(null, null, 'yyyy/MM/dd'));// 1398/09/04 
+console.log(shamsiDate.date(null, null, 'yyyy/MM/dd HH:mm:ss'));// 1398/09/04 09:05:51
+console.log(shamsiDate.date(new Date(), null, 'yyyy/MM/dd HH:mm:ss'));// 1398/09/04 09:05:51
+console.log(shamsiDate.date(null, "America/New_York", 'yyyy/MM/dd HH:mm:ss'));// 1398/09/04 09:05:51 
+console.log(shamsiDate.date(new Date(), "Asia/Tehran", 'yyyy/MM/dd HH:mm:ss'));// 1398/09/04 09:05:51
 
 console.log(shamsiDate.jalali('2019/10/25'));
 console.log(shamsiDate.jalali('2019/10/25 09:24:31'));
